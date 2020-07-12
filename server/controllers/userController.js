51a3632928c0ac6e1e42e13e54c1b07cb8ad1fdf
sastaachar <justinjohnmathew@karunya.edu.in>
2020-07-13 01:15:41 +0530

    [ 🚧 Work in progress : Api] Updated Model

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,11 @@
 // signup
+export const signupUser = async (req, res) => {};
 
 // login
+export const loginUser = async (req, res) => {};
 
 // signout
+export const signoutUser = async (req, res) => {};
 
 // deleteUser
+export const deleteUser = async (req, res) => {};
--- server/models/user.js
+++ server/models/user.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
+    required: false,
+  },
+  loginType: {
+    type: String,
     required: true,
   },
   profilePic: {
