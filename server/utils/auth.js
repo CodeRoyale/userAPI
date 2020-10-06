0aa3b066c4892c5f5ea8371b7f4b9cb63ed2f1e7
coming___soon <sachinvilasnagane@karunya.edu.in>
2020-10-06 19:15:41 +0530

    Update auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -52,7 +52,7 @@ const verifyToken = (token, key) => {
 const getCookieOptions = (TTL) => ({
   maxAge: TTL,
   httpOnly: true,
-  secure: false,
+  secure: process.env.NODE_ENV !== 'test',
   sameSite: 'None',
 });
 
